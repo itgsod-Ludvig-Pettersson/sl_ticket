b427d87d2af67faf4e92ecebd010ddb21ff95ff4
itgsod-Ludvig-Pettersson <ludvig.pettersson@elev.it-gymnasiet.se>
2017-10-25 13:17:59 +0200

    sl ticket. Ludvig

--- sl.se.py
+++ sl.se.py
@@ -1,11 +1,24 @@
-def fares(age, student=False, senior=False):
-    print age,student,senior
-    return 'hel'
+def fares(age,barn=False, senior=False, vuxen=False):
+    if age>=20 and age>20 and age<=65:
+        if barn==True:
+            return"price is 25kr"
+        elif senior==True:
+            return"price is 25kr"
+        else:
+            return"price is 50kr"
+    elif age <20 or age >65:
+        if barn==True:
+            return"price is 25kr"
+        elif senior==True:
+            return"price is 25kr"
+        elif barn==True:
+            return "price is 25kr"
+        else:
+            return"price is 25kr"
+
+print fares(65,senior=True)
+print fares(40,vuxen=False)
+print fares(15,barn=True)
 
 
 
-
-
-fares(10)
-fares(65,senior=True)
-fares(25,student=True)
